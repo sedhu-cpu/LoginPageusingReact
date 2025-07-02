b35ae5faae5bcabf5d8ee3bb98ba12b9a86d95b2
Sudhanshu Mishra <69859734+sedhu-cpu@users.noreply.github.com>
2025-07-02 18:37:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './components/LoginPage'
+import Dashboard from './components/Dashboard'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
